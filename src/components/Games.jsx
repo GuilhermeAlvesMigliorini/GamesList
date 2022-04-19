c1c4019606305852761f97fd57ccb3617cb72623
GuilhermeAlves <alvesmigliorini@hotmail.com>
2022-04-19 13:08:38 -0300

    request updated

--- src/components/Games.jsx
+++ src/components/Games.jsx
@@ -1,28 +1,32 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
 function Games() {
 
-  const [info, setInfo] = useState()
+  const [gameData, setgameData] = useState("loading...")
   const apiKey = "c348e769ad414a7d837ab9698b8abce2"
   const search = "monster-hunter-world"
 
-  const game = fetch('/api'+`?&key=${apiKey}&search=${search}`, {})
+  const game = fetch('/api'+`?&key=${apiKey}&search=${search}`)
     .then(resp => resp.json())
     .then((data) => {
       console.log(data)
-      setInfo(data.results.at(0))
+      setgameData(data.results.at(0))
     })
-    
+      if(gameData.name != null){
+        return(
+          <div>
+            <h1>{gameData.name}</h1>
+            <img src={gameData.background_image} alt={gameData.name} />
+          </div>
+          
+        )
 
-
-
-  return (
-    <div>
-      {info.name}
-      <img src={info.background_image} alt={info.name} />
-    </div>
-  )
+      } else{
+        return(
+          <h1>{gameData}</h1>
+        )
+      } 
 }
 
 export default Games
